2115e2e7fbf54a10c71d8480831e7a1fa1f341a3
blackarea <supersute142@gmail.com>
2023-07-24 20:57:11 +0900

    Docs : start.sh 수정

--- start.sh
+++ start.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 
-# start.sh
-# 서버 구동을 위한 스크립트
-
-ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)
-source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/project
 PROJECT_NAME=shimpyo  # 어플리케이션 이름 변경
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/deploy/*.jar $REPOSITORY/
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
@@ -29,18 +22,7 @@ else
     sleep 2
 fi
 
-echo "> 현재 8081 포트를 사용 중인 프로세스 확인"
-EXISTING_PID=$(lsof -t -i:8081)
-
-if [ -z "$EXISTING_PID" ]; then
-    echo "> 8081 포트를 사용 중인 프로세스가 없습니다."
-else
-    echo "> 8081 포트를 사용 중인 프로세스를 종료합니다. PID: $EXISTING_PID"
-    sudo kill -9 $EXISTING_PID
-    sleep 2
-fi
-
-cd /home/ubuntu/action
+cd /home/ec2-user/project
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -51,10 +33,6 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
-IDLE_PROFILE=$(find_idle_profile)
-
-echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
         -Dspring.config.location=classpath:/application.yml,/home/ec2-user/project/application-db.yml \
